9159e44ed5c76d0a54ce015fa875ff96f45e278c
garau <garau@LAPTOP-DJD1FPFE>
2026-05-24 20:40:22 -0600

    Desarrollo de sub clase, libro y revista

---
+++ Libro.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author garau
+ */
+public class Libro extends Material {
+    // Atributos propios
+    private String autor;
+    private int cantidadPaginas;
+    private Genero genero;
+
+    // Constructor
+    public Libro(int id,
+                 String titulo,
+                 String autor,
+                 int cantidadPaginas,
+                 Genero genero) {
+
+        // Constructor de la superclase
+        super(id, titulo);
+
+        // Inicializar atributos propios
+        this.autor = autor;
+        this.cantidadPaginas = cantidadPaginas;
+        this.genero = genero;
+    }
+
+    // Getters y Setters
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getCantidadPaginas() {
+        return cantidadPaginas;
+    }
+
+    public void setCantidadPaginas(int cantidadPaginas) {
+        this.cantidadPaginas = cantidadPaginas;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    // Sobrescribir método mostrarInfo
+    @Override
+    public void mostrarInfo() {
+
+        System.out.println("===== LIBRO =====");
+
+        // Información heredada
+        super.mostrarInfo();
+
+        // Información propia
+        System.out.println("Autor: " + autor);
+        System.out.println("Cantidad de páginas: " + cantidadPaginas);
+        System.out.println("Género: " + genero);
+    }
+}
---
+++ Revista.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author garau
+ */
+public class Revista extends Material {
+    // Atributos propios
+    private int numeroEdicion;
+    private String mesPublicacion;
+
+    // Constructor
+    public Revista(int id,
+                   String titulo,
+                   int numeroEdicion,
+                   String mesPublicacion) {
+
+        // Constructor de Material
+        super(id, titulo);
+
+        // Inicializar atributos
+        this.numeroEdicion = numeroEdicion;
+        this.mesPublicacion = mesPublicacion;
+    }
+
+    // Getters y Setters
+    public int getNumeroEdicion() {
+        return numeroEdicion;
+    }
+
+    public void setNumeroEdicion(int numeroEdicion) {
+        this.numeroEdicion = numeroEdicion;
+    }
+
+    public String getMesPublicacion() {
+        return mesPublicacion;
+    }
+
+    public void setMesPublicacion(String mesPublicacion) {
+        this.mesPublicacion = mesPublicacion;
+    }
+
+    // Sobrescribir método mostrarInfo
+    @Override
+    public void mostrarInfo() {
+
+        System.out.println("===== REVISTA =====");
+
+        // Información heredada
+        super.mostrarInfo();
+
+        // Información propia
+        System.out.println("Número de edición: " + numeroEdicion);
+        System.out.println("Mes de publicación: " + mesPublicacion);
+    } 
+}
+
